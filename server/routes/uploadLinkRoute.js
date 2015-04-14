15b3542b70b159878a6cd0d2ea79dabc2001a45a
Dmitry Vapelnik <dvapelnik@gmail.com>
2015-04-15 00:04:31 +0300

    1. Backend: code refactored

--- server/routes/uploadLinkRoute.js
+++ server/routes/uploadLinkRoute.js
@@ -11,40 +11,68 @@ module.exports = function (options) {
   return function (req, res) {
     logger.verbose('Responded: /upload/link');
 
-    async.waterfall([
-      function (callback) {
-        logger.verbose('Link');
-        logger.verbose(req.body.link);
+    if (req.body.link) {
+      var resCallback = req.body.link.match(/^magnet/) != null
+        ? getTorrentSourceFromMagnet
+        : getTorrentSourceFromRemoteFile;
 
-        //@todo Check is req.body.link not empty
+      console.log(resCallback);
 
-        httpTorrentDownloader(
-          req.body.link, config.uploadPath + '/' + req.session.id,
-          function (timeStamp) {
-            callback(null, timeStamp)
-          },
-          function (error) {
-            callback(error);
-          });
-      },
-      function (timestamp, callback) {
-        fs.readFile(
-          [config.uploadPath, req.session.id, timestamp + '.torrent'].join('/'),
-          function (error, data) {
-            if (error) {
+      resCallback(sourceCallbackImpl, errorCallbackImpl);
+    } else {
+      res.status(400).send('Bad request: link not defined');
+    }
+
+    function getTorrentSourceFromRemoteFile(sourceCallback, errorCallback) {
+      async.waterfall([
+        function (callback) {
+          logger.verbose('Link');
+          logger.verbose(req.body.link);
+
+          //@todo Check is request.body.link not empty
+
+          httpTorrentDownloader(
+            req.body.link, config.uploadPath + '/' + req.session.id,
+            function (timeStamp) {
+              callback(null, timeStamp)
+            },
+            function (error) {
               callback(error);
-            } else {
-              var torrentData = parseTorrent(data);
-              console.log(torrentData);
-              callback(null, torrentData);
-            }
-          });
-      }
-    ], function (error, result) {
-      if (error) {
-        logger.error(error);
-      }
-      res.json(result);
-    });
-  }
+            });
+        },
+        function (timestamp, callback) {
+          fs.readFile(
+            [config.uploadPath, req.session.id, timestamp + '.torrent'].join('/'),
+            function (error, data) {
+              if (error) {
+                callback(error);
+              } else {
+                callback(null, data);
+
+              }
+            });
+        }
+      ], function (error, source) {
+        if (error) {
+          errorCallback(error);
+        } else {
+          sourceCallback(source);
+        }
+      });
+    }
+
+    function getTorrentSourceFromMagnet(sourceCallback) {
+      sourceCallback(req.body.link);
+    }
+
+    function sourceCallbackImpl(source) {
+      var torrentParsedData = parseTorrent(source);
+
+      res.json(torrentParsedData);
+    }
+
+    function errorCallbackImpl(error) {
+      res.status(500).send('Error occurred');
+    }
+  };
 };
